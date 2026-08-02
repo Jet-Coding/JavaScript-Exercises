8fec95f717bf6169041c0c62301809cedbefcf14
Jet-Coding <sindoljerald@gmail.com>
2026-08-02 22:05:35 +0800

    uploading excercises to store quizes in different chapters of javascript programming tutorials.

---
+++ Chapter-1_Fundamentals/Unit-5_DataTypes/Quiz-1.js
@@ -0,0 +1,25 @@
+/* Introduction to JavaScript */
+//What does console.log() do in JavaScript?
+//Answer: Prints a message to the console.
+
+console.log("Hello, World!");
+console.log("Pizza");
+console.log("Hi");
+console.log("Sun");
+
+//What will this code print?
+console.log("cat");
+
+//What will this code print?
+console.log("Blue sky");
+
+//Put the lines in order so the code prints Ready, Set, Go.
+console.log("1.Ready");
+console.log("3.Go");
+console.log("2.Set");
+
+//Fill in the missing parts to print the text "Yes".
+("Yes");
+
+//Fill in the missing text so the code prints "Hello".
+console.log();
