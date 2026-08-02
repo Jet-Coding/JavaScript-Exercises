179c3fb50c6625a967080b8d9e016e07ad4ddae7
Jet-Coding <sindoljerald@gmail.com>
2026-08-02 20:34:40 +0800

    updating tutorials and projects

---
+++ tutorial.js
@@ -0,0 +1,28 @@
+/* While Loop */
+// What will this code print?
+let enemy = 3;
+while (enemy > 0) {
+	enemy = enemy - 1;
+}
+console.log(enemy);
+
+// Another while loop example
+let lives = 2;
+while (lives > 0) {
+	lives = lives - 1;
+}
+console.log(lives);
+
+// Another while loop example
+let fuel = 2;
+while (fuel > 0) {
+	fuel = fuel - 1;
+}
+console.log(fuel);
+
+// Another while loop example
+let count = 3;
+while (count > 0) {
+	count = count - 1;
+}
+console.log(count);
