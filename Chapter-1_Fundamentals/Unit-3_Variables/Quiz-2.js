ecf7199dcf1a03dc73453910dd46390f8522caea
Jet-Coding <sindoljerald@gmail.com>
2026-08-14 06:07:05 +0800

    store variable name.

---
+++ Chapter-1_Fundamentals/Unit-3_Variables/Quiz-2.js
@@ -0,0 +1,43 @@
+/* Variables */
+
+//What does a variable do in JavaScript?
+//Answer: It Store a value to use later.
+
+//what makes a JavaScript variable name clear and valid?
+//Answer: It uses simple words and starts with a letter,_,or $
+
+//Which line stores 7 in a clear, valid variable name?
+let appleCount = 7;
+console.log(appleCount);
+
+//Which line stores "Mia" in a beginner-friendly name?
+let playerName = "Mia";
+console.log(playerName);
+
+//Store the score 12, then print that same variable.
+let gameScore = 12;
+console.log(gameScore);
+
+//Save "blue" in clear color name, then print it.
+let favoriteColor = "blue";
+console.log(favoriteColor);
+
+//What will this code print?
+let petName = "Niko";
+console.log(petName);
+
+//What will this code print?
+let totalBooks = 4;
+console.log(totalBooks);
+
+//order the lines to store "Leo" in playerName and print it.
+let payerName = "Leo";
+console.log(playerName);
+
+//fill in the clear variable name for the number 9.
+let pageCount = 9;
+console.log(pageCount);
+
+//Use the same variabe name to print "Sora".
+let userName = "Sora";
+console.log(userName);
