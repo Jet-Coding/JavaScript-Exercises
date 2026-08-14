2920da6f9feb69199dfcdbe54cb25c90a6c49520
Jet-Coding <sindoljerald@gmail.com>
2026-08-14 18:56:34 +0800

    update the variable data.

---
+++ Chapter-1_Fundamentals/Unit-3_Variables/Quiz-3.js
@@ -0,0 +1,53 @@
+/* Variables */
+
+//What does a variable do in JavaScript?
+//Answer: It Store a value to use later.
+
+//What does it mean to update a variable in JavaScript?
+//Answer: Give the variable a new value after it already exist.
+
+//Which line changes score from 3 to 8 before printing?
+let score = 3;
+score = 8;
+console.log(score);
+
+//Which line changes the pet name to "Milo" before printing?
+let pet = "Nori";
+pet = "Milo";
+console.log(pet);
+
+//Change city from "Rome" to "Lima" before it prints.
+let city = "Rome";
+city = "Lima";
+console.log(city);
+
+//Change the number of cups from 2 to 5 before printing.
+let cups = 2;
+cups = 5;
+console.log(cups);
+
+//What will thhis code print?
+let color = "red";
+color = "blue";
+console.log(color);
+
+//What will this code print?
+let pages = 10;
+pages = 4;
+pages = 9;
+console.log(pages);
+
+//Order the line to setcoins to 1, change it to 6, then print 6.
+let coins = 1;
+coins = 6;
+console.log(coins);
+
+//Fill in the name to update snack to "apple" before printing.
+let snack = "cake";
+snack = "apple";
+console.log(snack);
+
+//Fill in the new number so level changes from 1 to 7.
+let level = 1;
+level = 7;
+console.log(level);
