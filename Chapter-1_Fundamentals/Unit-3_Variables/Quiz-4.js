8627388ca25813a8c6526ab1f5079deeecc2622e
Jet-Coding <sindoljerald@gmail.com>
2026-08-18 09:15:35 +0800

    Using Variable in output

---
+++ Chapter-1_Fundamentals/Unit-3_Variables/Quiz-4.js
@@ -0,0 +1,44 @@
+/* Variables */
+//Using Variable in output
+
+//What does a variable do in JavaScript?
+//Answer: It Store a value to use later.
+
+//What Does console.log() do in javascript?
+//Shows the stored value in output.
+
+//Which line prints the value stored in city?
+let city = "Paris";
+console.log(city);
+
+//Which line prints the number stored in score?
+let score = 9;
+console.log(score);
+
+//Print the value stored in petName.
+let petName = "Milo";
+console.log(petName);
+
+//Show the numbersaved in level, which is 4.
+let level = 4;
+console.log(level);
+
+//What will this code print?
+let drink = "water";
+console.log(drink);
+
+//What will this code print?
+let year = 2025;
+console.log(year);
+
+//Order the line to store "blue" in color, then print it.
+let color = "blue";
+console.log(color);
+
+//use the saved name to print what user holds.
+let user = "Ana";
+console.log(user);
+
+//Fill in the command that shows points in the output.
+let points = 15;
+console.log(points);
